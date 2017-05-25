a08f32c84349bb36cee5e6c6d2a427c148304bd0
Roger Melich <rogermelich@gmail.com>
2017-05-25 20:49:45 +0200

    Delete Boot

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import 'pixi'
 import 'p2'
 import Phaser from 'phaser'
 
-import BootState from './states/Boot'
 import SplashState from './states/Splash'
 import GameState from './states/Game'
 
@@ -16,7 +15,6 @@ class Game extends Phaser.Game {
 
     super(width, height, Phaser.CANVAS, 'content', null)
 
-    this.state.add('Boot', BootState, false)
     this.state.add('Splash', SplashState, false)
     this.state.add('Game', GameState, false)
 
