f6523cdd7effdd8e6849808472eb1f5213e68cd9
Roger Melich <rogermelich@gmail.com>
2017-05-30 20:13:02 +0200

    Add GameOver Sound

--- src/states/GameOver.js
+++ src/states/GameOver.js
@@ -2,12 +2,15 @@ export default class extends Phaser.State {
   preload() {
     this.stateText = game.add.text(400,200,' ', { fontSize: '24px', fill: '#FFFFFF' });
     this.stateText.text = "    Game Over \n Click to restart";
+    this.game.load.audio('game_over', './assets/audio/game_over.mp3')
   }
 
   create() {
     this.game.stage.backgroundColor = "#000"
     this.stateText.visible = true
     game.input.onTap.addOnce(this.startGame, this)
+    this.game_overSound = this.game.add.audio('game_over')
+    this.game_overSound.play()
   }
 
   startGame() {
